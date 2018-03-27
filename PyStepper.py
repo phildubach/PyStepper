43f7776fb6f77eb616ba2aad53eb34b395ccfc6e
Phil Dubach <phildubach@gmail.com>
2018-03-27 11:38:15 -0700

    Stepper daemon and test server

--- PyStepper.py
+++ PyStepper.py
@@ -21,18 +21,99 @@
 # SOFTWARE.
 
 import RPi.GPIO as Gpio
-import time
+import time, math
+from Queue import Queue
+from threading import Thread
+
+class PyStepperDaemon(Thread):
+    """Provides a server daemon that executes advanced movements"""
+
+    def __init__(self, stepper, position=0, max_speed=1000, max_accel=2000, upper_limit=None, lower_limit=None):
+        Thread.__init__(self)
+        self.daemon = True
+        self.stepper = stepper         # the stepper instance
+        self.shutdown = False          # flag to stop execution and exit
+        self.max_speed = max_speed     # maximum speed in steps/s
+        self.max_accel = max_accel     # maximum acceleration in steps/s/s
+        self.position = position       # current absolute position
+        self.target = position         # absolute target position
+        self.speed = 0                 # current speed
+        self.ulimit = upper_limit      # upper limit (steps from zero)
+        self.llimit = lower_limit      # lower limit (steps from zero)
+        self.tasks = Queue()           # movement queue
+        if self.ulimit != None and self.position > self.ulimit:
+            raise RuntimeException("position (%d) > upper limit (%d)" % (self.position, self.ulimit))
+        if self.llimit != None and self.position < self.llimit:
+            raise RuntimeException("position (%d) < lower limit (%d)" % (self.position, self.llimit))
+
+    def run(self):
+        """The daemon's main loop; should not be called directly!"""
+        while not self.shutdown:
+            (target, speed, accel, absolute) = self.tasks.get()
+            start = self.position
+            if not absolute:
+                target = start + target
+            self.target = target
+            # TODO: handle limits
+            dist = abs(target - start)
+            if not dist == 0:
+                sign = (target - start) / dist
+                done = 0
+                accel_time = float(speed) / accel
+                accel_dist = speed * accel_time / 2
+                if (2 * accel_dist > dist):
+                    # cannot accelerate to full speed
+                    accel_dist = dist / 2
+                    speed = math.sqrt(2 * accel_dist * accel)
+                    accel_time = float(speed) / accel
+                # acceleration loop
+                while done < accel_dist:
+                    self.speed = speed / accel_dist * (done + 1)
+                    incr_time = 1.0 / self.speed
+                    self.stepper.step(sign)
+                    self.position += sign
+                    time.sleep(incr_time)
+                    done += 1
+                # uniform speed
+                self.speed = speed
+                incr_time = 1.0 / self.speed
+                while done < (dist - accel_dist):
+                    self.stepper.step(sign)
+                    self.position += sign
+                    time.sleep(incr_time)
+                    done += 1
+                # deceleration loop
+                while done < dist:
+                    self.speed = speed / accel_dist * (dist - done)
+                    incr_time = 1.0 / self.speed
+                    self.stepper.step(sign)
+                    self.position += sign
+                    time.sleep(incr_time)
+                    done += 1
+            # movement completed
+            self.speed = 0
+            self.tasks.task_done()
+
+    def shutdown(self):
+        """Ask the daemon to shut down and wait for it to terminate"""
+        self.shutdown = True
+        self.join()
+
+    def queue(self, target, speed, accel, absolute=True):
+        if speed == 0:
+            speed = self.max_speed
+        if accel == 0:
+            accel = self.max_accel
+        self.tasks.put((target, speed, accel, absolute))
 
 class PyStepper:
     """Provides low-level functions for controlling a stepper motor connected to
     GPIO pins on a Raspberry Pi"""
 
-    UNIPOLAR = 0
-    BIPOLAR = 1
     FORWARD = 1
     BACKWARD = -1
 
-    sequences = [ [ # UNIPOLAR
+    sequences = { 'UNIPOLAR': [
                     [ True,  False, False, False ],
                     [ True,  True,  False, False ],
                     [ False, True,  False, False ],
@@ -41,7 +122,7 @@ class PyStepper:
                     [ False, False, True,  True  ],
                     [ False, False, False, True  ],
                     [ True,  False, False, True  ] ],
-                  [ # BIPOLAR
+                  'BIPOLAR': [
                     [ False, True,  False, True  ],
                     [ False, False, False, True  ],
                     [ True,  False, False, True  ],
@@ -50,23 +131,22 @@ class PyStepper:
                     [ False, False, True , False ],
                     [ False, True , True , False ],
                     [ False, True , False, False ] ]
-                ]
+                }
 
-
-    def __init__(self, sequence, pins):
+    def __init__(self, sequence_name, pins):
         """Create an instance of PyStepper.
 
         Arguments:
-        sequence -- either PyStepper.UNIPOLAR or PyStepper.BIPOLAR
-        pins     -- an array of 4 pin numbers referring to the pin numbers on
-                    the connector
+        sequence_name -- either 'UNIPOLAR' or 'BIPOLAR'
+        pins          -- an array of 4 pin numbers referring to the pin numbers on
+                         the connector
         """
         
-        self.sequence = PyStepper.sequences[sequence]
+        self.sequence = PyStepper.sequences[sequence_name]
         self.pins = pins
         Gpio.setmode(Gpio.BOARD)
-        Gpio.setwarnings(False)
         self.position = 0;
+        self._server = None
         for pin in self.pins:
             Gpio.setup(pin, Gpio.OUT)
             Gpio.output(pin, Gpio.LOW)
@@ -92,20 +172,58 @@ class PyStepper:
         """Convenience method to take one step backward"""
         self.step(self.BACKWARD)
 
+    def get_server(self):
+        if not self._server:
+            raise RuntimeError("Server not running")
+        return self._server
+
+    def start_daemon(self):
+        """Start a daemon thread for scheduling advanced movements"""
+        if self._server:
+            raise RuntimeError("Daemon already running")
+        self._server = PyStepperDaemon(self)
+        self._server.start()
+
+    def stop_daemon(self):
+        """Stop the previously started daemon thread"""
+        server = self.get_server()
+        server.stop()
+        self._server = None
+
+    def queue(self, target, speed=0, accel=0, absolute=True):
+        server = self.get_server()
+        server.queue(target, speed, accel, absolute)
+
+    def sync(self):
+        server = self.get_server()
+        server.tasks.join()
+
+    def status(self):
+        server = self.get_server()
+        return dict(position=server.position, target=server.target, speed=server.speed)
+
 if __name__ == '__main__':
     # Create an instance of PyStepper for a unipolar motor connected to the
     # pins given in the array. Note that the pin numbers refer to the numers
     # on the 40-pin connector/header, NOT the GPIO numbers
-    stepper = PyStepper(PyStepper.BIPOLAR, [ 40, 38, 36, 37 ])
+    stepper = PyStepper('BIPOLAR', [ 40, 38, 36, 37 ])
 
-    # Test by taking a number of steps
-    for i in range(200):
-	# arbitrarily choose to step forward
-        stepper.forward()
+    # deactivated test code
+    if False:
+        # Test by taking a number of steps
+        for i in range(200):
+            # arbitrarily choose to step forward
+            stepper.forward()
 
-	# sleep for 10 milliseconds between steps; this determines the speed of
-	# rotation; maximum speed depends on the motor and power supply
-        time.sleep(0.01)
+            # sleep for 10 milliseconds between steps; this determines the speed of
+            # rotation; maximum speed depends on the motor and power supply
+            time.sleep(0.01)
+
+    # start the daemon
+    stepper.start_daemon()
+    stepper.queue(800, speed=400, accel=200)
+    stepper.queue(400, speed=800)
+    stepper.sync()
 
     # Before exiting, clean up, redefining all pins as INPUTS
     Gpio.cleanup()
---
+++ TestServer.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+
+# Copyright 2018 Phil Dubach
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from PyStepper import PyStepper
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import os.path, json, sys, socket
+import RPi.GPIO as Gpio
+
+class GetResponse:
+    def __init__(self, handler):
+        self.handler = handler
+        path = self.handler.path
+        self.fname = None
+        self.json = None
+        self.ftype = 'text/plain'
+        self.flength = 0
+        self.code = 404
+        if path == '/':
+            path = '/static/index.html'
+        if path.startswith('/static/'):
+            self.fname = path[1:]
+            if os.path.isfile(self.fname):
+                self.code = 200
+                self.flength = os.path.getsize(self.fname)
+                if self.fname.endswith('.html'):
+                    self.ftype = 'text/html'
+                elif self.fname.endswith('.css'):
+                    self.ftype = 'text/css'
+                elif self.fname.endswith('.js'):
+                    self.ftype = 'text/javascript'
+        elif path.startswith('/api/'):
+            if path == '/api/status':
+                self.code = 200
+                status = self.handler.get_stepper().status()
+                self.json = json.dumps([
+                    { 'key': 'Current position', 'value': status['position'] },
+                    { 'key': 'Target position', 'value': status['target'] },
+                    { 'key': 'Current speed', 'value': status['speed'] }
+                ])
+                self.ftype = 'application/json'
+
+class RequestHandler(BaseHTTPRequestHandler):
+    def set_headers(self, code=200, content_type=None, content_length=None):
+        self.send_response(code)
+        if content_type:
+            self.send_header('Content-Type', content_type)
+        if content_length:
+            self.send_header('Content-Length', content_length)
+        self.end_headers()
+
+    def get_stepper(self):
+        return self.server.stepper
+
+    def do_GET(self):
+        response = GetResponse(self)
+        if response.code == 200:
+            if response.fname and response.flength:
+                self.set_headers(response.code, content_type=response.ftype, content_length=response.flength)
+                f = open(response.fname)
+                # TODO chunks?
+                data = f.read()
+                self.wfile.write(data)
+            elif response.json:
+                self.set_headers(response.code, content_type=response.ftype, content_length=len(response.json))
+                self.wfile.write(response.json)
+        else:
+            self.send_response(response.code)
+
+    def do_HEAD(self):
+        response = GetResponse(self)
+        if response.code == 200:
+            if response.fname and response.flength:
+                self.set_headers(response.code, response.ftype, response.flength)
+            elif response.json:
+                self.set_headers(response.code, content_type=response.ftype, content_length=len(response.json))
+        else:
+            self.send_response(response.code)
+        
+    def do_POST(self):
+        if self.path == '/api/move':
+            length = int(self.headers.getheader('Content-Length'))
+            json_data = self.rfile.read(length)
+            if True:
+                data = json.loads(json_data)
+                # required field
+                target = data['target_position']
+                # optional fields
+                speed = data.get('speed', 0)
+                accel = data.get('acceleration', 0)
+                absolute = data.get('absolute', True)
+                self.get_stepper().queue(target, speed, accel, absolute)
+                self.set_headers(code=204)
+            if False:
+                print "Error in post: ", sys.exc_info()[0]
+                self.set_headers(code=400, content_type="text/plain")
+                self.wfile.write("Invalid post data")
+        else:            
+            self.set_headers(404)
+        
+def run_server(stepper, port=8080):
+    server_address = ('', port)
+    print "".join(("Listening on http://", get_ip_addr(), ":", str(port), "/"))
+    print "Exit with <ctrl>-c"
+    server = HTTPServer(server_address, RequestHandler)
+    server.stepper = stepper
+    server.serve_forever()
+
+def get_ip_addr():
+    # TODO: there must be a better way to get the local IP address
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    addr = s.getsockname()[0]
+    s.close()
+    return addr
+
+if __name__ == "__main__":
+    stepper = PyStepper('BIPOLAR', [ 40, 38, 36, 37 ])
+    stepper.start_daemon()
+    try:
+        run_server(stepper)
+    except KeyboardInterrupt:
+        Gpio.cleanup()
